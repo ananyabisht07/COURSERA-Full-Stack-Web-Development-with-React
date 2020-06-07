3ea6d69747bc26c58e27d8d8db842c70cdc403ca
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-07 12:14:53 +0530

    Presentational and Container Components

--- Front-End Web Development with React/confusion/src/components/MenuComponent.js
+++ Front-End Web Development with React/confusion/src/components/MenuComponent.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishDetail from './DishDetail';
+
 
 class Menu extends Component{
 
-    constructor(props){
-        super(props);
-
-        this.state = {
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dish){
-        this.setState({
-            selectedDish: dish
-        });
-    }
+    
+    
 
     
 
@@ -24,7 +13,7 @@ class Menu extends Component{
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => this.onDishSelect(dish)} >
+                <Card onClick={() => this.props.onClick(dish.id)} >
                     <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
@@ -37,9 +26,6 @@ class Menu extends Component{
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                    <DishDetail dish={this.state.selectedDish}/>
                 </div>
             </div>
         );
