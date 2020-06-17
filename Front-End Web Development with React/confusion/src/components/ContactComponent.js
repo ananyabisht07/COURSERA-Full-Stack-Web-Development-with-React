326b23872988a660b8a8c2bf27958dbdbb7b0170
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-17 17:41:00 +0530

    React Redux Forms Revisited

--- Front-End Web Development with React/confusion/src/components/ContactComponent.js
+++ Front-End Web Development with React/confusion/src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 const required = (val) => val && val.length;
@@ -83,6 +83,7 @@ class Contact extends Component {
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
     render(){
@@ -131,7 +132,7 @@ class Contact extends Component {
                    <h3>Send us your Feedback</h3>
                 </div>
                  <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                         <Row className="form-group">
                             <Label htmlFor="firstname" md={2}>First Name</Label>
                             <Col md={10}>
@@ -176,7 +177,8 @@ class Contact extends Component {
                                 />
                             </Col>                        
                         </Row>
-                        <Row className="form-group">                        <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                        <Row className="form-group">                        
+                            <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
                             <Col md={10}>
                                 <Control.text model=".telnum" id="telnum" name="telnum"
                                     placeholder="Tel. number"
@@ -256,7 +258,7 @@ class Contact extends Component {
                                 </Button>
                             </Col>
                         </Row>
-                    </LocalForm>
+                    </Form>
                  </div>
             </div>
         </div>
