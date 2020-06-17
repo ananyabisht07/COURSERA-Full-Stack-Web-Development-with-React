0b5c30c6ca2cc075330857f52f939a6aac58965d
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-18 00:01:02 +0530

    Fetch Post Comment

--- Front-End Web Development with React/confusion/src/components/DishDetail.js
+++ Front-End Web Development with React/confusion/src/components/DishDetail.js
@@ -23,7 +23,7 @@ function RenderDish({dish}) {
 
 
     
-    function RenderComments({comments, addComment, dishId})  {
+    function RenderComments({comments, postComment, dishId})  {
 
 
         if (comments != null)
@@ -44,7 +44,7 @@ function RenderDish({dish}) {
                             );
                         })}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
         else 
@@ -93,7 +93,7 @@ function RenderDish({dish}) {
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id} />
                         
                         
