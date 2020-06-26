f79999209efcaf0ccf5e81c806d7f13db3fc1c4c
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-26 16:47:02 +0530

    Navigation Part-3

--- COURSERA-React Native/Components/HomeComponent.js
+++ COURSERA-React Native/Components/HomeComponent.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, ScrollView, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../Shared/dishes';
+import { PROMOTIONS } from '../Shared/promotions';
+import { LEADERS } from '../Shared/leaders';
+
+function RenderItem(props) {
+    console.log(props)
+    const item = props.item;
+    
+    if (item != null) {
+        return(
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation}
+                image={require('./images/uthappizza.png')}>
+                <Text
+                    style={{margin: 10}}>
+                    {item.description}</Text>
+            </Card>
+        );
+    }
+    else {
+        return(<View></View>);
+    }
+}
 
 class Home extends Component {
 
-    static navigationOptions = {
-        title: 'Home'
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+          dishes: DISHES,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
+        };
+    }
 
     render() {
         return(
-            <View><Text>Home Component</Text></View>
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            </ScrollView>
         );
     }
 }
