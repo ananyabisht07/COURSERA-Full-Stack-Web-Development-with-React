ed40ae5843878da54aeb6f549f1c89c7fea9f4fd
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-29 13:05:04 +0530

    Gesture Part-2

--- COURSERA-React Native/Components/DishdetailComponent.js
+++ COURSERA-React Native/Components/DishdetailComponent.js
@@ -60,6 +60,9 @@ function RenderComments(props) {
 function RenderDish(props) {
 
     const dish = props.dish;
+    
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if ( dx < -200 )
             return true;
@@ -70,6 +73,11 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
+        },
+
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
             if (recognizeDrag(gestureState))
@@ -90,7 +98,7 @@ function RenderDish(props) {
     
         if (dish != null) {
             return(
-                <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers}>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000} ref={this.handleViewRef} {...panResponder.panHandlers}>
                     <Card
                     featuredTitle={dish.name}
                     image={{uri: baseUrl + dish.image}}>
