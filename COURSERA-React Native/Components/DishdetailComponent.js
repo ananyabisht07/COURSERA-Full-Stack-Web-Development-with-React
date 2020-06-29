b3827d6d10912292c8891c2d93ad5f41c4b3b22e
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-29 12:52:34 +0530

    Gestures Part 1

--- COURSERA-React Native/Components/DishdetailComponent.js
+++ COURSERA-React Native/Components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, PanResponder ,Alert} from 'react-native';
 import { Card, Icon, Input, Rating } from "react-native-elements";
 import * as Animatable from 'react-native-animatable';
 //import { Icon } from 'react-native-paper/lib/typescript/src/components/Avatar/Avatar';
@@ -60,10 +60,37 @@ function RenderComments(props) {
 function RenderDish(props) {
 
     const dish = props.dish;
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+        if ( dx < -200 )
+            return true;
+        else
+            return false;
+    }
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log("pan responder end", gestureState);
+            if (recognizeDrag(gestureState))
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                    {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
+                    ],
+                    { cancelable: false }
+                );
+
+            return true;
+        }
+    })
+
     
         if (dish != null) {
             return(
-                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers}>
                     <Card
                     featuredTitle={dish.name}
                     image={{uri: baseUrl + dish.image}}>
